7d0bc74a16ff8f575718608dc9e87587005c6641
khainee <khingzay797@gmail.com>
2023-01-20 14:58:23 +0630

    update

--- update.py
+++ update.py
@@ -37,7 +37,7 @@ try:
     if len(UPSTREAM_REPO) == 0:
        raise TypeError
 except:
-    UPSTREAM_REPO = "https://github.com/codewithweeb/mirror-with-weeb"
+    UPSTREAM_REPO = "https://github.com/khainee/render-mltb"
 try:
     if len(UPSTREAM_BRANCH) == 0:
        raise TypeError
@@ -48,8 +48,8 @@ if ospath.exists('.git'):
     srun(["rm", "-rf", ".git"])
 
 update = srun([f"git init -q \
-                 && git config --global user.email codewithweeb@gmail.com \
-                 && git config --global user.name Karan \
+                 && git config --global user.email khingzay797@gmail.com \
+                 && git config --global user.name khainee \
                  && git add . \
                  && git commit -sm update -q \
                  && git remote add origin {UPSTREAM_REPO} \
@@ -58,5 +58,10 @@ update = srun([f"git init -q \
 
 if update.returncode == 0:
     log_info('Successfully updated with latest commit from UPSTREAM_REPO')
+    log_info(f'Upstream Repo: {UPSTREAM_REPO}')
+    log_info(f'Upstream Branch: {UPSTREAM_BRANCH}')
 else:
     log_error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
+    log_info(f'Entered Upstream Repo: {UPSTREAM_REPO}')
+    log_info(f'Entered Upstream Branch: {UPSTREAM_BRANCH}')
+
