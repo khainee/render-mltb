3d9ec6f23a6a47c4ddf3a2033be61e7326ba3f9a
khainee <118953358+khainee@users.noreply.github.com>
2023-01-23 13:55:27 +0630

    Update update.py

--- update.py
+++ update.py
@@ -42,3 +42,4 @@ if update.returncode == 0:
     log_info('Successfully updated with latest commit from UPSTREAM_REPO')
 else:
     log_error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
+srun(["python3", "get_config.py"])
