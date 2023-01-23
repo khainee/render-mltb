2e7a77be78f45f11555dd082462145f200fb2d3a
khainee <118953358+khainee@users.noreply.github.com>
2023-01-23 13:44:28 +0630

    Update update.py

--- update.py
+++ update.py
@@ -42,21 +42,3 @@ if update.returncode == 0:
     log_info('Successfully updated with latest commit from UPSTREAM_REPO')
 else:
     log_error('Something went wrong while updating, check UPSTREAM_REPO if valid or not!')
-
-CONFIG_FILE_URL = environ.get('CONFIG_FILE_URL')
-try:
-    if len(CONFIG_FILE_URL) == 0:
-        raise TypeError
-    try:
-        res = rget(CONFIG_FILE_URL)
-        if res.status_code == 200:
-            with open('config.env', 'wb+') as f:
-                f.write(res.content)
-        else:
-            log_error(f"Failed to download config.env {res.status_code}")
-    except Exception as e:
-        log_error(f"CONFIG_FILE_URL: {e}")
-except:
-    pass
-
-load_dotenv('config.env', override=True)
